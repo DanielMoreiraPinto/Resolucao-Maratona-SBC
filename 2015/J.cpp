de59d01ba17431fbe3eb625aff0cedfedbb2fb6b
DanielMoreiraPinto <danielmoreirapinto.ti@gmail.com>
2020-03-09 10:35:28 -0300

    Questão está certa, porém o URI indica 40% de erro

--- 2015/J.cpp
+++ 2015/J.cpp
@@ -5,21 +5,34 @@ using namespace std;
 
 int main() {
 
-    int j, r, n;
+    int jg, r, n, maior=-1, vencedor;
     vector<vector<int>> m;
+    vector<int> tp;
 
-    cin >> j >> r;
+    cin >> jg >> r;
 
-    for(int i=0; i < j; i++) {
+    for(int i=0; i < jg; i++) {
+        tp.push_back(0);
+    }
+
+    for(int i=0; i < jg; i++) {
         vector<int> aux;
-        for(int k=0; k < r; k++) {
+        for(int j=0; j < r; j++) {
             cin >> n;
             aux.push_back(n);
+            tp[i] += n;
         }
         m.push_back(aux);
     }
 
-    
+    for(int i=0; i < jg; i++) {
+        if(tp[i] >= maior) {
+            maior = tp[i];
+            vencedor = i;
+        }
+    }
+    vencedor++;
+    cout << vencedor;
 
     return 0;
 }
